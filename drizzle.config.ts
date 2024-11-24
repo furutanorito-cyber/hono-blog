8cf002a1c32fdeb2fc82b26c2ddc5f7380d7b77f
furutanorito-cyber <furutanorito@gmail.com>
2024-11-24 08:01:02 +0000

    ローカルのDBへの操作・確認をできるようにした

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,14 +3,22 @@ import * as dotenv from "dotenv";
 
 dotenv.config();
 
-export default {
-	schema: "./app/schema/*",
-	out: "./migrations",
-	dialect: "sqlite",
-	driver: "d1-http",
-	dbCredentials: {
-		accountId: process.env.CLOUDFLARE_ACCOUNT_ID,
-		databaseId: process.env.CLOUDFLARE_DATABASE_ID,
-		token: process.env.CLOUDFLARE_WORKERS_TOKEN,
-	},
-} satisfies Config;
+export default process.env.LOCAL_DB_PATH
+	? ({
+			schema: "./app/schema/*",
+			dialect: "sqlite",
+			dbCredentials: {
+				url: process.env.LOCAL_DB_PATH,
+			},
+		} satisfies Config)
+	: ({
+			schema: "./app/schema/*",
+			out: "./migrations",
+			dialect: "sqlite",
+			driver: "d1-http",
+			dbCredentials: {
+				accountId: process.env.CLOUDFLARE_ACCOUNT_ID,
+				databaseId: process.env.CLOUDFLARE_DATABASE_ID,
+				token: process.env.CLOUDFLARE_WORKERS_TOKEN,
+			},
+		} satisfies Config);
